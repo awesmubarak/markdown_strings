995f47c4e9ed84f8a0655ab816858c1cd3f55598
Awes Mubarak <awes.mubarak@awesmubarak.com>
2017-04-29 15:21:03 +0100

    Remove some language classifiers

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as file:
 
 setup(
      name='markdown_strings',
-     version='1.0.0',
+     version='1.0.1',
      description='Create markdown-formatted text',
      long_description=long_description,
      url='https://github.com/abactel/markdown_strings',
@@ -23,22 +23,7 @@ setup(
          'Operating System :: OS Independent'
          'Topic :: Text Processing :: Markup'
          'License :: OSI Approved :: MIT License',
-         # check which of these it acually works on:
          'Programming Language :: Python'
-         'Programming Language :: Python :: 2'
-         'Programming Language :: Python :: 2.3'
-         'Programming Language :: Python :: 2.4'
-         'Programming Language :: Python :: 2.5'
-         'Programming Language :: Python :: 2.6'
-         'Programming Language :: Python :: 2.7'
-         'Programming Language :: Python :: 3'
-         'Programming Language :: Python :: 3.0'
-         'Programming Language :: Python :: 3.1'
-         'Programming Language :: Python :: 3.2'
-         'Programming Language :: Python :: 3.3'
-         'Programming Language :: Python :: 3.4'
-         'Programming Language :: Python :: 3.5'
-         'Programming Language :: Python :: 3.6'
      ],
      keywords='markdown md',
      packages=['markdown_strings']
