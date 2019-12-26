29f50fdb9919182d1af9f2db1c9fd586fa5883a5
Awes Mubarak <contact@awesmubarak.com>
2019-12-26 14:29:37 +0000

    Add changelog and bump version number

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [3.2.0] - 2019-12-26
+
+### Added
+
+-   `table_from_rows` creates tables from lists containing each row
+
+## Changed
+
+-   Columns of the table can contain non-strings
+-   The setex header's heading characters are now the same length as the heading text.
+
+[3.2.0]: https://github.com/awesmubarak/gitget/releases/tag/v3.2.0
--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, "README.rst"), encoding="utf-8") as file:
+with open(path.join(here, "README.md"), encoding="utf-8") as file:
     long_description = file.read()
 
 setup(
     name="markdown_strings",
-    version="3.1.1",
+    version="3.2.1",
     description="Create markdown formatted text",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/awesmubarak/markdown_strings",
     author="Awes Mubarak",
-    author_email="awes.mubarak@awesmubarak.com",
+    author_email="contact@awesmubarak.com",
     license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
